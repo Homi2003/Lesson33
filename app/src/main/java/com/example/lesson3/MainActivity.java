c2c7a87be25ac5c8b8f701a4ecab76ecc9b8ce14
Akmaral <makosya1234567@gmail.com>
2020-11-06 13:04:58 +0600

    new one with edittext

--- app/src/main/java/com/example/lesson3/MainActivity.java
+++ app/src/main/java/com/example/lesson3/MainActivity.java
@@ -6,29 +6,34 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etText;
     TextView textView;
     Button btnCLicked;
+    String str;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.button_on_click);
+
+        etText = findViewById(R.id.etText);
         textView = findViewById(R.id.tvtext);
         btnCLicked = findViewById(R.id.btn);
 
-        View.OnClickListener oc1 = new View.OnClickListener() {
+        btnCLicked.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setTextColor(Color.RED);
+                String str = etText.getText().toString();
+                textView.setText(str);
             }
-        };
-        btnCLicked.setOnClickListener(oc1);
-    }
+        });
 
-    public void btn(View view) {
-        textView.setText("Clicked");
 
     }
-}
+
+
+
+    }
